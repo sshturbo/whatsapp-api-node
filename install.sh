80df2219fd4f41c4ae813d925383563e347f32d5
Jefferson Hipolito <149891602+sshturbo@users.noreply.github.com>
2024-11-12 16:16:18 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -133,9 +133,21 @@ while true; do
 done
 
 # Verifica se o contêiner Docker já está em execução e remove-o
-if [ "$(docker ps -q -f name=evolution_api)" ]; then
-    run_with_spinner "docker stop evolution_api >/dev/null 2>&1" "Parando contêiner Docker existente"
-    run_with_spinner "docker rm evolution_api >/dev/null 2>&1" "Removendo contêiner Docker existente"
+if [ "$(docker ps -q -f name=evolution_v2)" ]; then
+    run_with_spinner "docker stop evolution_v2 >/dev/null 2>&1" "Parando contêiner evolution_v2 existente"
+    run_with_spinner "docker rm evolution_v2 >/dev/null 2>&1" "Removendo contêiner evolution_v2 existente"
+fi
+if [ "$(docker ps -q -f name=pgadmin)" ]; then
+    run_with_spinner "docker stop pgadmin >/dev/null 2>&1" "Parando contêiner pgadmin existente"
+    run_with_spinner "docker rm pgadmin >/dev/null 2>&1" "Removendo contêiner pgadmin existente"
+fi
+if [ "$(docker ps -q -f name=postgres_db)" ]; then
+    run_with_spinner "docker stop postgres_db >/dev/null 2>&1" "Parando contêiner postgres_db existente"
+    run_with_spinner "docker rm postgres_db >/dev/null 2>&1" "Removendo contêiner postgres_db existente"
+fi
+if [ "$(docker ps -q -f name=redis_service)" ]; then
+    run_with_spinner "docker stop redis_service >/dev/null 2>&1" "Parando contêiner redis_service existente"
+    run_with_spinner "docker rm redis_service >/dev/null 2>&1" "Removendo contêiner redis_service existente"
 fi
 
 # Verifica se o aplicativo PM2 já está em execução e remove-o
@@ -167,21 +179,40 @@ run_with_spinner "npm start >/dev/null 2>&1" "Iniciando aplicação"
 cd evolution || exit_with_error "Falha ao acessar diretório do repositório"
 
 # Modifica o arquivo docker-compose.yml com a porta fornecida pelo usuário
-show_progress "Modificando docker-compose.yml"
+show_progress "Modificando a porta da evolution para $PORT"
 sed -i "s|8080:8080|$PORT:8080|" docker-compose.yml || exit_with_error "Falha ao modificar o docker-compose.yml"
 
-# Renomeia o arquivo exemple.env para .env
-run_with_spinner "mv exemple.env .env" "Renomeando exemple.env para .env"
+# Adiciona a AUTHENTICATION_API_KEY no arquivo  docker-compose.yml
+show_progress "Adicionando AUTHENTICATION_API_KEY no arquivo docker-compose.yml"
+sed -i "s|AUTHENTICATION_API_KEY=cb8c87193f183f91a16e6eb697ca84ec|AUTHENTICATION_API_KEY=${AUTHENTICATION_API_KEY}|" docker-compose.yml || exit_with_error "Falha ao modificar o docker-compose.yml"
 
-# Adiciona a AUTHENTICATION_API_KEY no arquivo .env
-show_progress "Adicionando AUTHENTICATION_API_KEY no arquivo .env"
-echo "AUTHENTICATION_API_KEY=${AUTHENTICATION_API_KEY}" >> .env
+# Adiciona a POSTGRES_PASSWORD no arquivo  docker-compose.yml
+show_progress "Adicionando POSTGRES_PASSWORD no arquivo docker-compose.yml"
+sed -i "s|POSTGRES_PASSWORD: cb8c87193f183f91a16e6eb697ca84ec|POSTGRES_PASSWORD: ${AUTHENTICATION_API_KEY}|" docker-compose.yml || exit_with_error "Falha ao modificar o docker-compose.yml"
+
+# Adiciona a PGADMIN_DEFAULT_PASSWORD no arquivo  docker-compose.yml
+show_progress "Adicionando PGADMIN_DEFAULT_PASSWORD no arquivo  docker-compose.yml"
+sed -i "s|PGADMIN_DEFAULT_PASSWORD: cb8c87193f183f91a16e6eb697ca84ec|PGADMIN_DEFAULT_PASSWORD: ${AUTHENTICATION_API_KEY}|" docker-compose.yml || exit_with_error "Falha ao modificar o docker-compose.yml"
+
+# Adiciona a SERVER_URL no arquivo docker-compose.yml
+show_progress "Adicionando SERVER_URL no arquivo docker-compose.yml"
+sed -i "s|SERVER_URL=http://localhost:8080|SERVER_URL=http://localhost:$PORT|" docker-compose.yml || exit_with_error "Falha ao modificar o docker-compose.yml"
+
+# Adiciona a DATABASE_CONNECTION_URI no arquivo docker-compose.yml
+show_progress "Adicionando DATABASE_CONNECTION_URI no arquivo docker-compose.yml"
+sed -i "s|DATABASE_CONNECTION_URI=postgresql://postgres:cb8c87193f183f91a16e6eb697ca84ec@postgres_db:5432/evolution|DATABASE_CONNECTION_URI=postgresql://postgres:${AUTHENTICATION_API_KEY}@postgres_db:5432/evolution|" docker-compose.yml || exit_with_error "Falha ao modificar o docker-compose.yml"
 
 # Executa o Docker Compose para subir o contêiner
 show_progress "Iniciando contêiner do evolutin"
 docker-compose up -d >/dev/null 2>&1
 
+IPV4=$(curl -s https://api.ipify.org)
 
 echo "------------------------------------------"
 echo "- Instalação concluída e aplicação iniciada"
+echo""
+echo"URL da api evolution: http:$IPV4:$PORT"
+echo"URL da documentação da evolution: https://doc.evolution-api.com/v2/api-reference/get-information"
+echo"URL da Painel de gerenciamento: http:$IPV4:21468"
+echo""
 echo "------------------------------------------"
